24d53c45eaa9901e1904af27681a03d53bc48ef4
Nicolas <oyarzun.nicolas@gmail.com>
2024-11-17 16:26:38 -0300

    libreria random + assets entregados

--- Assets/Code/bola.cs
+++ Assets/Code/bola.cs
@@ -1,3 +1,5 @@
+using TMPro;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,11 +9,23 @@ public class bulletSpeed : MonoBehaviour
     bool puedoLanzar =true;
     [SerializeField] float fuerzaImpulso = 10;
     [SerializeField] GameObject pala;
-    
+    //private float anguloX = UnityEngine.Random.Range((float)0.8, 1);
+    //private float anguloY = UnityEngine.Random.Range((float)0.7, 1);
+    private int vidas = 3;
+    private int puntaje= 0;
+    private int HighScore= 0;
+        [SerializeField] private TextMeshProUGUI textoScore;
+    [SerializeField] private TextMeshProUGUI textoVida;
+    [SerializeField] private TextMeshProUGUI textoHighScore;
+
+
     void Start()
     {
        rb = this.gameObject.GetComponent<Rigidbody2D>();
-       
+        textoVida.text = "Vidas: " + vidas;
+        textoScore.text = puntaje + " :Puntaje";
+        textoHighScore.text = "HighScore: " + HighScore;
+
     }
 
     // Update is called once per frame
@@ -20,7 +34,9 @@ public class bulletSpeed : MonoBehaviour
        if (Input.GetKeyDown(KeyCode.Space) && puedoLanzar == true){
             transform.SetParent(null);
             rb.bodyType=RigidbodyType2D.Dynamic;
-            rb.AddForce(new Vector2(1, 1).normalized * fuerzaImpulso, ForceMode2D.Impulse);
+            // nunca funciona con valores random
+            //rb.AddForce(new Vector2(anguloX,anguloY).normalized * fuerzaImpulso, ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(1,1).normalized * fuerzaImpulso, ForceMode2D.Impulse);
             puedoLanzar =false; 
        } 
     }
@@ -30,7 +46,15 @@ public class bulletSpeed : MonoBehaviour
         if (otro.gameObject.CompareTag("bloque"))
         {
             Destroy(otro.gameObject);
-        };
+            puntaje++;
+            textoScore.text = puntaje + " :Puntaje";
+            if (puntaje > HighScore)
+            {
+                HighScore = puntaje;
+                textoHighScore.text = "HighScore: "+ HighScore;
+
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -48,5 +72,13 @@ public class bulletSpeed : MonoBehaviour
         transform.SetParent(pala.transform);
         transform.localPosition = new Vector3(0, 1, 0);
         puedoLanzar = true;
+        vidas--;
+        puntaje = 0;
+        textoScore.text = puntaje + " :Puntaje";
+        textoVida.text = "Vidas: " + vidas;
+       if (vidas == 0) { 
+            //muere
+        }
+        
     }
 }
